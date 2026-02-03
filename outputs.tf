fb2c9af00555255f277cdf42e93767c56b8726b1
tejaswigsb8 <tejaswi.gsb8@gmail.com>
2026-02-03 05:35:47 +0000

    Adding outputs for my name and job position

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+# outputs.tf
+
+output "applicant_name" {
+  description = "Applicant’s name"
+  value       = "Venkata Tejaswi Gutti"
+}
+
+output "position_applied" {
+  description = "Position you are applying for"
+  value       = "Senior DevOps Engineer"
+}
